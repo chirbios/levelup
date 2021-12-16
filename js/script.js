afabb518ad54838393d615b3a6c9a2bdef77e0aa
Alex <kazzon95@gmail.com>
2021-12-16 17:53:04 +0300

    update form and some changes

--- js/script.js
+++ js/script.js
@@ -68,4 +68,10 @@ $(document).ready(function(){
       behavior: 'smooth', // и плавно 
     });
   });
+
+  $('[name=tel]').bind("change keyup input click", function() {
+    if (this.value.match(/[^0-9]/g)) {
+    this.value = this.value.replace(/[^0-9]/g, '');
+    }
+    });
 });
